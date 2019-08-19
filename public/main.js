0b77ee86806bfc13c3c4e5f95bd4c4f790ebc395
Rick Ortiz <rick@Ricks-MacBook-Pro.local>
2019-08-18 22:55:51 -0400

    made a few changes

--- public/main.js
+++ public/main.js
@@ -1,7 +1,25 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+const main = async () => {
+  getImage()
+  getLaunchData()
+}
+
+const imgAPI = 'https://sdg-astro-api.herokuapp.com/api/Nasa/apod'
+const launchesAPI =
+  'https://sdg-astro-api.herokuapp.com/api/SpaceX/launches/upcoming'
+
+const getImage = async () => {
+  const response = await fetch(imgAPI)
+  const data = await response.json()
+  document.querySelector('.header-image').style.backgroundImage = `'url(${
+    data.hdUrl
+  })'`
+  console.log(data.hdUrl)
+}
+
+const getLaunchData = async () => {
+  const response = await fetch(launchesAPI)
+  const data = await response.json()
+  console.log(data)
 }
 
 document.addEventListener('DOMContentLoaded', main)
